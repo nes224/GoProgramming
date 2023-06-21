664e8a5d764ead7acfbf0eebefad2613fcf80527
Nes224 <Nes224@hotmail.com>
2023-06-21 10:28:08 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,11 @@
+package main 
+import ("fmt")
+
+func main(){
+	var student1 string = "nes"
+	var student2 string = "nes224"
+	x := 2
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
+}
