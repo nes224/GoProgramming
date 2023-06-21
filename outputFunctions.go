983e3665cca53cda755325d3b44ec96e1237da49
Nes224 <Nes224@hotmail.com>
2023-06-21 10:37:58 +0700

    goProgramming

---
+++ outputFunctions.go
@@ -0,0 +1,10 @@
+package main 
+import ("fmt")
+func main(){
+	var i,j string = "hello","world"
+	var n string = "nes224"
+	x := 20
+	fmt.Println(i,"\n",j)
+	fmt.Printf("n has value: %v and type %T\n",n,n)
+	fmt.Printf("x has value: %v and type %T\n",x,x)
+}
